b8eded743c2ebee453369d1edc74a8afa308f860
Huy Dang <tamthieuxz@gmail.com>
2022-02-06 20:47:29 +0700

    Add 3 news route create-new-user edit-user delete-user and test response success

--- src/services/userService.js
+++ src/services/userService.js
@@ -96,27 +96,28 @@ let createNewUser = (data) => {
       //check email is exist ???
       let check = await checkUserEmail(data.email)
       if (check === true) {
-        return resolve({
+        resolve({
           errCode: 1,
-          message: "Your email is already in use, Plz try another email!",
+          errMessage: "Your email is already in use, Plz try another email!",
+        })
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          gender: data.gender === "1" ? true : false,
+          phoneNumber: data.phoneNumber,
+          roleId: data.roleId,
+        })
+
+        resolve({
+          errCode: 0,
+          errMessage: "OK",
         })
       }
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        gender: data.gender === "1" ? true : false,
-        phoneNumber: data.phoneNumber,
-        roleId: data.roleId,
-      })
-
-      resolve({
-        errCode: 0,
-        message: "OK",
-      })
     } catch (error) {
       reject(error)
     }
